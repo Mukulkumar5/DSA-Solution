ec0d22ea47a2266268987982510684f739899add
Mukul Kumar <mukulkumar7486@gmail.com>
2026-08-01 18:50:31 +0530

    Home work question.

---
+++ src/mix/basic/HQuestions.java
@@ -0,0 +1,41 @@
+package mix.basic;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class HQuestions {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getAlternateEle(new int[]{1, 2, 3, 4})));
+        List<Integer> listOfele1 = new ArrayList<>();
+        listOfele1.add(1);
+        listOfele1.add(2);
+        listOfele1.add(3);
+        listOfele1.add(4);
+
+        List<Integer> listOfele2 = new ArrayList<>();
+        listOfele2.add(2);
+        listOfele2.add(5);
+        listOfele2.add(6);
+        listOfele2.add(4);
+        System.out.println(intersectionEle(listOfele1, listOfele2));
+    }
+    // change into alternate array
+    public static int[] getAlternateEle(int []ele){
+        for(int i=0; i<ele.length-1;i= i+2){
+            int k = ele[i];
+            ele[i] = ele[i+1];
+            ele[i+1] = k;
+        }
+        return ele;
+    }
+
+     public static List<Integer> intersectionEle(List<Integer> listOfele1, List<Integer> listOfele2){
+        // list contains check on every element
+       // listOfele2.stream().filter(listOfele1::contains).toList();
+
+        // set contains works based on hashcode calculation directly goes to bucket where number exist
+         Set<Integer> sEle = new HashSet<>(listOfele2);
+        return listOfele1.stream().filter(sEle::contains).toList();
+    }
+
+}
